154d882edf79cfe05090ecf42d689e4b41b988ec
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-24 18:50:50 +0200

    component refactoring

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
 import { useState, useCallback } from "react";
-import { useForm , FieldValues , SubmitHandler } from "react-hook-form";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 type Variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
 	const [variant, setVariant] = useState<Variant>("LOGIN");
@@ -10,28 +12,61 @@ const AuthForm = () => {
 		setVariant(variant === "LOGIN" ? "REGISTER" : "LOGIN");
 	}, [variant]);
 
-  const { register , handleSubmit, formState : { errors , } } = useForm<FieldValues>({
-    defaultValues : {
-      name : "",
-      email  : "",
-      password : "",
-    }
-  })
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setLoading(true);
-    if(variant === 'LOGIN'){
-      // next auth sign in
-    }
-    if(variant === 'REGISTER'){
-      // register a new user
-    }
-  }
-  const socialAction = (action : string) => {
-    setLoading(true);
-    //side in with next auth providers
-  
-  }
-	return <div>AuthForm</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FieldValues>({
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+	});
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		setLoading(true);
+		if (variant === "LOGIN") {
+			// next auth sign in
+		}
+		if (variant === "REGISTER") {
+			// register a new user
+		}
+	};
+	const socialAction = (action: string) => {
+		setLoading(true);
+		//side in with next auth providers
+	};
+	return (
+		<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+			<div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+				<form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+					{variant === "REGISTER" && (
+						<Input id="name" label="Name" register={register} errors={errors} />
+					)}
+					<Input
+						id="email"
+						label="Email"
+						type="email"
+						type="email"
+						register={register}
+						errors={errors}
+					/>
+					<Input
+						id="password"
+						label="password"
+						type="password"
+						register={register}
+						errors={errors}
+					/>
+					<div>
+						<Button>
+							Button
+						</Button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default AuthForm;
